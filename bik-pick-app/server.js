0b0e13d1e2971648be057e4a31aa158b06540cc6
Merp <nadia.ruszkiewicz@gmail.com>
2021-12-12 00:11:10 +0100

    updates to deploy to heroku

--- bik-pick-app/server.js
+++ bik-pick-app/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/BikPick/bik-pick-app'));
+app.use(express.static(__dirname + '/dist/<bik-pick-app>'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/BikPick/bik-pick-app/index.html'));});
+'/dist/BikPick/<bik-pick-app>/index.html'));});
 app.listen(process.env.PORT || 8080);
